21b5b0052f081e2efe37994add86223a0444d5a4
Emmanuelle Kamwa <emmanuellekamwa@gmail.com>
2025-08-13 10:18:51 +0100

    Created ChatboxIcon.jsx file in component folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 const App = () => {
-  return <div>App</div>;
+  return <div className="container">
+    <div className="chatbot-popup">
+      <div className="chat-header">
+        <div className="header-info">
+          <h2 className="logo-text">Chatbot</h2>
+        </div>
+      </div>
+    </div>
+  </div>;
 }
  
 export default App;
